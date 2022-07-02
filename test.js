ddd961aa99b12297c52b35cfe21e2979be57528d
shyBBy - laptop <dawid.a.olczak@gmail.com>
2022-07-02 23:19:43 +0200

    Added dashboard main view and count total bookings etc

--- test.js
+++ test.js
@@ -3,24 +3,47 @@
 
 const doctor1 = {
   name: 'Doctor1',
-  places: [`123dupa`, `123kupa`,]
+  places: [`123dupa`, `123kupa`,],
+  status: 'pending'
 }
 
 const doctor2 = {
   name: `Doctor2`,
-  places: [`jacek2`, `123dupa`]
+  places: [`jacek2`, `123dupa`],
+  status: 'active'
 }
 
+const doctor3 = {
+  name: `Doctor2`,
+  places: [`jacek2`, `123dupa`],
+  status: 'active'
+}
+const doctor4 = {
+  name: `Doctor2`,
+  places: [`jacek2`, `123dupa`],
+  status: 'pending'
+}
+const doctor5 = {
+  name: `Doctor2`,
+  places: [`jacek2`, `123dupa`],
+  status: 'active'
+}
 
-const doctors = [doctor1, doctor2]
+const doctors = [doctor1, doctor2, doctor3, doctor4, doctor5]
 
-const checkPlaces = (value) => {
-  if (value === `123dupa`) {
-    console.log(`w if: ${value}`)
+
+const pendings = [];
+const actives = [];
+
+for (let i = 0; i < doctors.length; i++){
+  if (doctors[i].status === 'pending'){
+    pendings.push('pending')
+  } else if(doctors[i].status === 'active'){
+    actives.push('active')
   }
-  return value === `123dupa`
-}
+};
 
-const forEach = doctors.forEach(doctor => {
-  console.log(doctor.places.filter(checkPlaces))
-})
+console.log(`Liczba elementow o statusie pending: ${pendings.length}`)
+console.log(`Liczba elementow o statusie active: ${actives.length}`)
+
+
